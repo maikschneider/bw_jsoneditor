99efb9f4f912eebf97ac2c93b84fb2ef7023c1f2
Maik Schneider <m.schneider@blueways.de>
2021-06-10 11:38:36 +0200

    feat: add cleanup script, bitbucket pipeline

---
+++ Build/Scripts/cleanUp.sh
@@ -0,0 +1,13 @@
+echo "Cleaning up.."
+rm -rf Documentation
+rm .gitignore
+rm -rf .ddev
+rm bitbucket-pipelines.yml
+rm composer.lock
+rm package.json
+rm tsconfig.json
+rm -rf Build
+rm package-lock.json
+rm -rf Resources/Private/TypeScript
+rm Gruntfile.js
+rm -rf config
