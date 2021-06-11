619cf6b383114c4cc844a2fd30fd11e654169a75
Maik Schneider <m.schneider@blueways.de>
2021-06-11 10:21:44 +0200

    feat: tested in typo3 v8 & v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.20-9.9.99',
+            'typo3' => '8.7.20-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
