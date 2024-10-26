af018272a3b3afb2930efd7b7f0c66b754e15646
Maik Schneider <mas@xima.de>
2024-10-27 00:14:31 +0200

    feat: remove typo3 v8-v11 support, remove grunt, typescript

--- Resources/Public/JavaScript/JsonForm.js
+++ Resources/Public/JavaScript/JsonForm.js
@@ -1,25 +1,33 @@
-define(["require", "exports", "TYPO3/CMS/BwJsoneditor/jsoneditor", "jquery"], function (require, exports, JSONEditor, $) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var JsonForm = (function () {
-        function JsonForm(itemFormElName) {
-            var element = $('.jsoneditor-form[data-input="' + itemFormElName + '"]').get(0);
-            var options = JSON.parse(decodeURIComponent($(element).attr('data-options')));
-            var hiddenInput = $('input[name="' + itemFormElName + '"]');
-            var json = $(hiddenInput).val();
-            if (!json)
-                json = '{}';
-            try {
-                json = JSON.parse(json);
-            }
-            catch (e) {
-            }
-            options.onChangeText = function (json) {
-                hiddenInput.val(json);
-            };
-            new JSONEditor(element, options, json);
-        }
-        return JsonForm;
-    }());
-    exports.JsonForm = JsonForm;
-});
+import {createJSONEditor} from 'vanilla-jsoneditor/standalone.js'
+
+export class JsonForm {
+
+  constructor(itemFormElName) {
+
+    console.log('JsonForm', itemFormElName);
+
+    const element = document.querySelector('.jsoneditor-form[data-input="' + itemFormElName + '"]');
+    const options = JSON.parse(decodeURIComponent(element.getAttribute('data-options')));
+    const hiddenInput = document.querySelector('input[name="' + itemFormElName + '"]');
+
+    let json = $(hiddenInput).val();
+    if (!json) json = '{}';
+
+    try {
+      json = JSON.parse(json);
+    } catch (e) {
+    }
+
+    options.onChangeText = function (json) {
+      hiddenInput.val(json);
+    };
+
+    createJSONEditor({
+      target: element,
+      props: {
+        json
+      }
+    })
+  }
+
+}
